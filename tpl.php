49328b9f7df19f913deb71776740def49b6b753f
yangqinjiang <531553280@qq.com>
2016-05-11 10:55:22 +0800

    修改模板

--- tpl.php
+++ tpl.php
@@ -16,7 +16,8 @@
  <ul>
  	<?php 
  		foreach ($raw_msg as $k => $v) {
- 			echo "<li>{$v['msg']} <a title='已解决' href='/kill/".$who."/".$k."'>x</a></li>";
+
+ 			echo "<li><a title='已解决' href='/kill/".$who."/".$k."'>x</a><pre>{$v['msg']} </pre></li>";
  		}
 
  	 ?>
