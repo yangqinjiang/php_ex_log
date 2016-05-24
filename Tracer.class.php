346ddb0320ccc35fd33e532d1b95d4ef9aa2617d
yangqinjiang <531553280@qq.com>
2016-05-24 12:01:35 +0800

    过滤信息

--- Tracer.class.php
+++ Tracer.class.php
@@ -26,17 +26,35 @@ class Tracer
         
         $ex_postid = $this->redis->incr('global:ex_postid:'.$prefix);
         $data['time']=time();//记录时间
-        $data['msg'] = array('id'=>$ex_postid,'msg'=>$data['msg']);
+        $msg = $data['msg'];
+        $this->sendMsg($msg);//根据权重发送信息
+        $data['msg'] = array('id'=>$ex_postid,'msg'=>$msg);
         $data['msg'] = json_encode($data['msg']);
         $this->redis->hMset('ex_post:postid:'.$prefix.':'.$ex_postid,$data);
         //时间集合
         $this->redis->zAdd('ex_post:'.$prefix,time(),$ex_postid);
+
+
     }
     public function kill($who,$key)
     {
         $this->redis->zRem('ex_post:'.$who,$key);
         $k = 'ex_post:postid:'.$who.':'.$key;
         $this->redis->del($k);
+    }
+    public function sendMsg($msg)
+    {
+        // echo $msg;
+        $out = '';
+        preg_match_all('/QBGOO-->((.*)[GTRNCOGL]):\s/x',$str2,$out);
+        if(empty($out[1])){//匹配不成功
+            return;
+        }
+        //判断
+        $trace_type =  $out[1];
+        if(in_array($trace_type,array('EMERG','ALERT','CRIT','ERR','WARN','NOTIC'))){//EMERG  ERR
+            // 发送信息
+        }
     }
     public function __destruct()
     {
