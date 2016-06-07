139213ec7d0b2fb3434fb45976ab2bbfba3bf1e6
yangqinjiang <531553280@qq.com>
2016-06-07 15:03:30 +0800

    测试worktile的应用开发

--- index.php
+++ index.php
@@ -113,10 +113,10 @@ $app->get('/worktile/response',function ($request, $response, $args)
 	}
 	$code = $_GET['code'];
 	var_dump($code);
-	$url = 'https://api.worktile.com/oauth2/access_token?client_id=2b4ddbd6f526434285f62b0006cebc0f&&client_secret=3d6b481a3dc04bf183651e062cbfc0e6&code='.$code;
+	$url = 'https://api.worktile.com/oauth2/access_token?client_id=2b4ddbd6f526434285f62b0006cebc0f&client_secret=3d6b481a3dc04bf183651e062cbfc0e6&code='.$code;
 	$ch = curl_init();
         $this_header = array(
-            "Content-Type"=>'application/json'
+            'Content-Type:application/json'
         );
 
     curl_setopt($ch,CURLOPT_HTTPHEADER,$this_header);
