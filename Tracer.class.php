4e3bc7fe4d4a33aff66c6247e4749b48f8fb7cef
yangqinjiang <531553280@qq.com>
2016-06-07 15:25:39 +0800

    测试worktile的应用开发

--- Tracer.class.php
+++ Tracer.class.php
@@ -45,9 +45,9 @@ class Tracer
     //设置标志位
     public function archive($who,$key)
     {
-        $this->redis->zRem('ex_post:'.$who,$key);
-        $k = 'ex_post:postid:'.$who.':'.$key;
-        $this->redis->del($k);
+        // $this->redis->zRem('ex_post:'.$who,$key);
+        // $k = 'ex_post:postid:'.$who.':'.$key;
+        // $this->redis->del($k);
         //hash set
         $this->redis->hMSet($k,['archive'=>1]);
     }
