65c298e43386fa01dc5e093fc99d5b6d554ef41a
yangqinjiang <531553280@qq.com>
2016-06-08 15:34:47 +0800

    测试worktile API

--- config.online.php
+++ config.online.php
@@ -12,6 +12,6 @@ $config['redis_db']['auth']   = "";
 $config['redis_db']['db_id']   = 1;
 
 $config['worktile']['client_id'] = '2b4ddbd6f526434285f62b0006cebc0f';
-$config['worktile']['client_secret'] = '2b4ddbd6f526434285f62b0006cebc0f';
+$config['worktile']['client_secret'] = '3d6b481a3dc04bf183651e062cbfc0e6';
 
 define('MEMBER_OK', 1);//会员状态,正常
